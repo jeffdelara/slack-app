698a29cd2815b5352665c255d45c3d528856f36d
Jeffrey de Lara <jeffreytdelara@gmail.com>
2021-11-21 12:17:10 +0800

    Sidebar displays direct messages on chat window.

--- src/components/DirectMessage.js
+++ src/components/DirectMessage.js
@@ -1,37 +1,94 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const DirectMessage = (props) => {
     // channelId is also the receiver_id to be used for fetching API
-    const {channelId} = props;
+    const {channelId, channelName} = props;
+    const receiverId = channelId;
+    const receiverName = channelName;
 
-    console.log(channelId);
+    const [messages, setMessages] = useState({});
+
+    console.log(receiverName);
+
+    const headers = {
+         accessToken : localStorage.getItem('access-token'),
+         client : localStorage.getItem('client'),
+         expiry : localStorage.getItem('expiry'),
+         uid : localStorage.getItem('uid'),
+         user : JSON.parse(localStorage.getItem('user'))
+    }
+
+    useEffect(() => {
+        console.log(receiverId);
+        getUserMessages(receiverId, headers);
+
+    }, [receiverId]);
+
+    const getUserMessages = (receiverId, headers) => {
+        const options = {
+            method: 'GET', 
+            mode: 'cors',
+            headers: {
+                'access-token' : headers.accessToken, 
+                'client' : headers.client, 
+                'expiry' : headers.expiry, 
+                'uid' : headers.uid
+            }
+        }
+
+        const url = `${process.env.REACT_APP_SLACK_ENDPOINT}/messages?receiver_id=${receiverId}&receiver_class=User`;
+        fetch(url, options)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data.data);
+                const dm = data.data; 
+
+                // const filteredMessages = dm.map(item => {
+                //     const tempDm = {}
+                //     tempDm[item.id] = {
+                //         sender: item.sender.email, 
+                //         receiver: item.receiver.email, 
+                //         body: item.body,
+                //         created_at: item.receiver.created_at
+                //     }
+                //     return tempDm;
+                // });
+
+                const filteredMessages = {};
+                for(const item of dm) {
+                    filteredMessages[item.id] = {
+                        sender: item.sender.email, 
+                        receiver: item.receiver.email, 
+                        body: item.body,
+                        created_at: item.receiver.created_at
+                    }
+                }
+
+                console.log(filteredMessages);
+
+                setMessages({...filteredMessages});
+            })
+    }
+
+    const chatMessages = Object.keys(messages).map(msg => {
+        return <ChannelMessage title={messages[msg].sender} 
+            message={messages[msg].body} 
+            created_at={messages[msg].created_at} />
+    })
     
     return (
         <section id="channel-chat">
             <div id="channel-header">
                 <div className="container header">
-                    <h1><img src="https://a.slack-edge.com/d4111/img/apps/workflows_192.png" alt="" /> Slackbot</h1>
+                    <h1><img src="https://a.slack-edge.com/d4111/img/apps/workflows_192.png" alt="" /> {channelName}</h1>
                 </div>
             </div>
 
             <div id="channel-chat-content">
-                <div className="channel-message">
-                    <div className="sender-pic"><img src="https://a.slack-edge.com/d4111/img/apps/workflows_192.png" alt="" /></div>
-                    <div className="sender">
-                        <div className="sender-name">Team Standup B13 <span className="created">8:01 PM</span></div>
-                        <div className="sender-message">Good evening! Don't forget to post your standup.</div>
-                        
-                    </div>
-                </div>
-
-                <div className="channel-message">
-                    <div className="sender-pic"><img src="https://ca.slack-edge.com/T010DU0GZE0-U02C42FABUK-8daed97695af-512" alt="" /></div>
-                    <div className="sender">
-                        <div className="sender-name">Jeff de Lara <span className="created">8:01 PM</span></div>
-                        <div className="sender-message">Good evening! Don't forget to post your standup.</div>
-
-                    </div>
-                </div>
+                {chatMessages}
+                {/* <ChannelMessage title={title} message={message} /> */}
             </div>
 
             <div id="channel-chat-input">
@@ -40,6 +97,20 @@ const DirectMessage = (props) => {
                 </div>
             </div>
         </section>
+    )
+}
+
+const ChannelMessage = (props) => {
+    const {title, message, created_at} = props;
+
+    return (
+        <div className="channel-message">
+            <div className="sender-pic"><img src="https://a.slack-edge.com/d4111/img/apps/workflows_192.png" alt="" /></div>
+            <div className="sender">
+                <div className="sender-name">{title} <span className="created">{created_at}</span></div>
+                <div className="sender-message">{message}</div>
+            </div>
+        </div>
     )
 }
 
