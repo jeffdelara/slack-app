52cc06fb39798b185b35cae172201a0b0f3ef743
Jeffrey de Lara <jeffreytdelara@gmail.com>
2021-12-06 13:52:18 +0800

    Updates

---
+++ precache-manifest.4e3699be627e6a35e3472de5872c865c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b178f8be7a23289e8e3862cad41b2e33",
+    "url": "/slack-app/index.html"
+  },
+  {
+    "revision": "519bb747fb06beb85024",
+    "url": "/slack-app/static/css/main.b0e9f7b2.chunk.css"
+  },
+  {
+    "revision": "1e78a4681c47f7117ff3",
+    "url": "/slack-app/static/js/2.5efb1266.chunk.js"
+  },
+  {
+    "revision": "252af85abbed85800d31",
+    "url": "/slack-app/static/js/3.e28df362.chunk.js"
+  },
+  {
+    "revision": "519bb747fb06beb85024",
+    "url": "/slack-app/static/js/main.5fd996dc.chunk.js"
+  },
+  {
+    "revision": "7a5f6c4d9045f0e1ac15",
+    "url": "/slack-app/static/js/runtime-main.8c628b86.js"
+  }
+]);
